3e8c770c3c537dfe2a88491a7fbe74e1318362de
Riley Martine <maxm215@gmail.com>
2018-02-27 18:48:55 -0500

    Make default name work

--- remindwindows/api.py
+++ remindwindows/api.py
@@ -5,10 +5,17 @@ import argparse
 import sys
 import re
 import hashlib
-import tabulate
 import subprocess
 from pathlib import Path
+import tabulate
 from remindme import REMIND_DIR
+
+
+class DefaultName(argparse.Action):
+    """Have a default filename that is created from the reminder argument."""
+    def __call__(self, parser, namespace, values, option_string=None):
+        namespace.fpath = text_to_fpath(values)
+        namespace.reminder = values
 
 def parse_args(args):
     """Parse CLI arguments via argparse and return the parsed args."""
@@ -16,29 +23,30 @@ def parse_args(args):
     subparsers = parser.add_subparsers(help="sub-command help", dest="cmd")
 
     parser_add = subparsers.add_parser('add', help='Add a New Reminder')
-    parser_add.add_argument('reminder', type=str, help='The text of the reminder')
+    parser_add.add_argument('reminder', type=str, action=DefaultName,
+                            help='The text of the reminder')
     parser_add.add_argument('-n', '--filename', type=not_reminder, required=False,
-        dest='fpath', help='The filename you want to use. ".rem" will be appended.')
-    parser_add.add_argument('-f', '--force', required=False, 
-        help='Clobber file with existing name')
+                            dest='fpath',
+                            help='The filename you want to use. ".rem" will be appended.')
 
     parser_list = subparsers.add_parser('list', aliases=['ls'], help='List reminder files')
 
-    parser_show = subparsers.add_parser('show', aliases=['cat'], 
-        help='Show a reminder, by filename or index.')
+    parser_show = subparsers.add_parser('show', aliases=['cat'],
+                                        help='Show a reminder, by filename or index.')
     parser_show.add_argument('file', type=is_reminder,
-        help='The filename or index of the reminder you want to view')
+                             help='The filename or index of the reminder you want to view')
 
     parser_delete = subparsers.add_parser('delete', aliases=['rm', 'del'],
-        help='Delete a reminder file')
-    parser_delete.add_argument('file', type=is_reminder,
-        help='The filename or index of the reminder you want to delete')
+                                          help='Delete a reminder file')
+    parser_delete.add_argument('files', type=is_reminder, nargs='*',
+                               help='The filename(s) or index(es) of the reminder(s) you\
+                               want to delete')
     parser_delete.add_argument('-f', '--force', action='store_true',
-        help='Do not prompt for deletion')
+                               help='Do not prompt for deletion')
 
-    parser_edit = subparsers.add_parser('edit', help='Edit an existing reminder')
+    parser_edit = subparsers.add_parser('edit', aliases=['vim'], help='Edit an existing reminder')
     parser_edit.add_argument('file', type=is_reminder,
-        help='The filename or index of the reminder you want to edit')
+                             help='The filename or index of the reminder you want to edit')
 
     if not args:
         parser.print_help()
@@ -52,19 +60,14 @@ def run_args(params):
         if params.fpath is None:
             params.fpath = text_to_fpath(params.reminder)
         add_reminder(params.reminder, params.fpath)
-    elif params.cmd == 'list':
+    elif params.cmd in ['list', 'ls']:
         list_reminders()
-    elif params.cmd == 'show':
+    elif params.cmd in ['show', 'cat']:
         show_reminder(params.file)
-    elif params.cmd == 'delete':
-        if params.force:
-            delete_reminder(params.file)
-            return
-        delete_str = input(f"Delete {params.file.name}? (Y/n): ")
-        if delete_str in ['y', 'Y', '']:
-            delete_reminder(params.file)
-    elif params.cmd == 'edit':
-       edit_reminder(params.file) 
+    elif params.cmd in ['delete', 'rm', 'del']:
+        delete_reminders(params.files, params.force)
+    elif params.cmd in ['edit', 'vim']:
+        edit_reminder(params.file)
 
 
 def edit_reminder(path):
@@ -89,7 +92,7 @@ def not_reminder(file):
     """Detect if a file is not an existing reminder"""
     path = resolve_reminder(file)
     if path.exists():
-        msg = f"{path.name} is already a reminder file. Use -f to override."
+        msg = f"{path.name} is already a reminder file."
         raise argparse.ArgumentTypeError(msg)
     return path
 
@@ -98,7 +101,7 @@ def resolve_reminder(file):
     Turn an index or filename into a path.
     Can be passed arguments in the form '0', 'remind.rem', 'remind'
     """
-    if type(file) == int:
+    if isinstance(file, int):
         file = str(file)
 
     if file.endswith('.rem'):
@@ -111,7 +114,7 @@ def resolve_reminder(file):
             raise argparse.ArgumentTypeError(msg)
     else:
         fpath = REMIND_DIR / (file + '.rem')
-    
+
     return Path(fpath)
 
 def list_reminders():
@@ -120,9 +123,16 @@ def list_reminders():
     indexed = list(zip(range(len(files)), files))
     print(tabulate.tabulate(indexed))
 
-def delete_reminder(path):
+def delete_reminders(files, force):
     """Delete a reminder file."""
-    path.unlink()
+    if force:
+        for file in files:
+            file.unlink()
+        return
+    for file in files:
+        delete_str = input(f"Delete {file.name}? (Y/n): ")
+        if delete_str in ['y', 'Y', '']:
+            file.unlink()
 
 def show_reminder(path):
     """Given an index or reminder file, display it."""
@@ -164,10 +174,6 @@ def add_reminder(text, fpath):
 
 
 if __name__ == '__main__':
-    params = parse_args(sys.argv[1:])
-    print(params)
-    run_args(params)
-
-
-
-
+    parsed = parse_args(sys.argv[1:])
+    print(parsed)
+    run_args(parsed)
