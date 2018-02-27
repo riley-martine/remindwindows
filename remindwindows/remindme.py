bc158a09d3cdb6c2000d4c819a2ce03a5e124e4b
Riley Martine <riley.martine@protonmail.com>
2018-02-27 12:35:41 -0500

    Add api functionality

--- remindwindows/remindme.py
+++ remindwindows/remindme.py
@@ -26,32 +26,8 @@ else:
     REMIND_DIR.mkdir()
 
 
-def text_to_fpath(text):
-    """Turns a reminder text into a suitable filename."""
-    EXTENSION = ".rem"
-    MAX_LEN = 20        # Not including extension
-
-    pattern = re.compile(r'[\W_]+', re.UNICODE) # Restrict to alphanumeric
-    alphanum = pattern.sub("", text)
-    shortened = alphanum[:MAX_LEN]             # Restrict length
-    if len(shortened) < 1:                     # If the reminder is something silly like "@@@@@"
-        shortened = hashlib.sha1(text.encode("utf8")).hexdigest()[:MAX_LEN]
-    fname = shortened + EXTENSION
-    fpath = REMIND_DIR.joinpath(fname)         # Create path
-
-    # We should add a number to the end of the filename if it already exists
-    num_rems = 0
-    WIDTH = 3 # Digits
-    while fpath.exists():
-        fname = shortened[:MAX_LEN-WIDTH] + str(num_rems).zfill(WIDTH) + EXTENSION
-        fpath = REMIND_DIR.joinpath(fname)
-        num_rems += 1
-
-    return fpath
-
-
 class Reminder(QWidget):
-    """QWidget that has a path, can be deleted, and displays itself."""
+    """QWidget that has a Path, can be deleted, and displays itself."""
     text = ""
     path = None
     label = None
@@ -116,13 +92,6 @@ class Reminder(QWidget):
         geometry.moveCenter(center)
         self.move(geometry.topLeft())
 
-
-def add_reminder(text):
-    """Create a reminder file with text $text."""
-    textpath = text_to_fpath(text)
-    textpath.touch()
-    with textpath.open('w') as reminder_file:
-        reminder_file.write(text)
 
 def get_current_reminders():
     """Get a list of Reminder objects for all current reminders."""
